c0fa1c971fcb055b233e83c314786a0524d28ff8
Sergey Dolganov <sclinede@gmail.com>
2016-09-18 20:37:11 +0500

    Graphs view. Dirty

--- lib/ossert/web.rb
+++ lib/ossert/web.rb
@@ -33,6 +33,74 @@ module Ossert
         slim :index
       end
 
+      get '/last_year_graph/:metric' do
+        @quarters_start_date = 1.year.ago
+        @quarters_end_date = Time.now.utc
+
+        @projects = params[:projects].split(',').map do |name|
+          project = Ossert::Project.load_by_name(name)
+          return "Project Not Found" unless project
+
+          Ossert::ProjectDecorator.new(project)
+        end
+
+        @community_metrics = @projects.first.community_quarter(@quarters_end_date).keys
+        @agility_metrics = @projects.first.agility_quarter(@quarters_end_date).keys
+
+        @metric_type = if @community_metrics.include?(params[:metric])
+                    :community
+                  elsif @agility_metrics.include?(params[:metric])
+                    :agility
+                  else
+                    raise "Metric Not Found"
+                  end
+        @metric = params[:metric]
+
+        slim :graph_show, layout: false
+      end
+
+      get '/total_graph/:metric' do
+        @quarters_start_date = Time.now.utc
+        @quarters_end_date = 20.years.ago
+
+        @projects = params[:projects].split(',').map do |name|
+          project = Ossert::Project.load_by_name(name)
+          return "Project Not Found" unless project
+
+          agility_start_date = Time.now.utc
+          agility_end_date = 20.years.ago
+          community_start_date = Time.now.utc
+          community_end_date = 20.years.ago
+
+          project.agility.quarters.fullfill!
+          agility_start_date = [project.agility.quarters.start_date, agility_start_date].min
+          agility_end_date = [project.agility.quarters.end_date, agility_end_date].max
+
+          project.community.quarters.fullfill!
+          community_start_date = [project.community.quarters.start_date, community_start_date].min
+          community_end_date = [project.community.quarters.end_date, community_end_date].max
+
+          @quarters_start_date = [@quarters_start_date, agility_start_date, community_start_date].min
+          @quarters_end_date = [@quarters_end_date, agility_end_date, community_end_date].max
+
+          Ossert::ProjectDecorator.new(project)
+        end
+
+        @community_metrics = @projects.first.community_quarter(@quarters_end_date).keys
+        @agility_metrics = @projects.first.agility_quarter(@quarters_end_date).keys
+
+        @metric_type = if @community_metrics.include?(params[:metric])
+                    :community
+                  elsif @agility_metrics.include?(params[:metric])
+                    :agility
+                  else
+                    raise "Metric Not Found"
+                  end
+        @metric = params[:metric]
+
+        slim :graph_show, layout: false
+      end
+
       get '/:name' do
         @project = Ossert::Project.load_by_name(params[:name])
         return "Not Found" unless @project
---
+++ public/js/example.js
@@ -0,0 +1,7 @@
+// Run the script on DOM ready:
+$(function(){
+ $('table').visualize({type: 'pie', height: '400px', width: '1200px'});
+	$('table').visualize({type: 'bar', width: '1200px'});
+	$('table').visualize({type: 'area', width: '1200px'});
+	$('table').visualize({type: 'line', width: '1200px'});
+});
