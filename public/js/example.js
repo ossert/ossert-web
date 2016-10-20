e0ea6275b4557616d85cf83f6c9be4463530000b
Sergey Dolganov <sclinede@gmail.com>
2016-10-20 20:07:52 +0500

    More graphs

--- public/js/example.js
+++ public/js/example.js
@@ -2,6 +2,6 @@
 $(function(){
  $('table').visualize({type: 'pie', height: '400px', width: '1200px'});
 	$('table').visualize({type: 'bar', width: '1200px'});
-	// $('table').visualize({type: 'area', width: '1200px'});
-	// $('table').visualize({type: 'line', width: '1200px'});
+	$('table').visualize({type: 'area', width: '1200px'});
+	$('table').visualize({type: 'line', width: '1200px'});
 });
