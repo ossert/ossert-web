c7a082ef20f76b4ea0973e61f8b077148743bb82
Sergey Dolganov <sclinede@gmail.com>
2016-11-06 16:42:41 +0500

    Fix: random to more popular, search_header

--- lib/ossert/web.rb
+++ lib/ossert/web.rb
@@ -62,12 +62,11 @@ module Ossert
       enable :sessions
 
       set :warmup, Warmup.perform
-      set :header_search, true
 
       get '/' do
         @warn = session[:warn]
         session[:warn] = nil
-        settings.header_search = false
+        @hide_header_search = true
 
         erb :index
       end
