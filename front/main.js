a45c0a989a82299a63c02601b57855a7a76b980c
Pavel Grinchenko <psdcoder@gmail.com>
2016-12-10 23:15:46 +0300

    Update gem-table styles. Write component to show charts for each metric in rows.

--- front/main.js
+++ front/main.js
@@ -3,10 +3,15 @@ import rafThrottle from 'raf-throttle';
 import { onScroll } from './blocks/gem-header';
 import { init as helpTooltipInit } from './blocks/help-tooltip';
 import { draw as drawTableMainChart } from './blocks/gem-stats-chart';
+import { renderTableCharts } from './blocks/gem-table-chart';
 import { smoothAnchorScrolling } from './blocks/link';
-import { isMobileView } from './blocks/utils';
+import { isMobileView, arrayFromNodes } from './blocks/utils';
 import searchForm from './blocks/search';
 import { gemDescriptionCollapser } from './blocks/gem';
+
+if (process.env.NODE_ENV === 'development') {
+  window.$ = $;
+}
 
 $(() => {
   if ($('#sticky-project-header').length) {
@@ -24,5 +29,14 @@ $(() => {
   gemDescriptionCollapser();
   $('.gem-stats-chart').each(function onEachChart() {
     drawTableMainChart(this, $(this).data('chart'));
+  });
+
+  arrayFromNodes(document.querySelectorAll('.js-gems-stats-table')).forEach(table => {
+    renderTableCharts({
+      titleNode: table.querySelector('.js-gems-stats-table__period-title'),
+      chartsNodes: table.querySelectorAll('.js-gems-stats-table__row-chart'),
+      statsCellNodes: table.querySelectorAll('.js-gems-stats-table__cell-stats'),
+      maxQuarters: JSON.parse(table.dataset.maxQuarters)
+    });
   });
 });
