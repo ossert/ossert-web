c219ec7ccbda1755e82471c04add5a8dde2bf0f6
K. Volchenko <likeath@gmail.com>
2016-12-13 21:46:26 +0300

    Feature: add Rubygems search suggestions (pt.2 – rework search classes, clean up view, fixes)

--- lib/ossert/web/projects_search.rb
+++ lib/ossert/web/projects_search.rb
@@ -1,58 +1,27 @@
 module Ossert
   module Web
-    module ProjectsSearch
-      module_function
+    class ProjectsSearch
+      attr_accessor :on_error
 
-      def by_name(name)
-        TotalSearchResults.new(
-          Searcher::Local.new(name),
-          Searcher::Rubygems.new(name)
-        )
+      def initialize(project_name)
+        @project_name = project_name
+        yield self if block_given?
       end
 
-      class TotalSearchResults
-        def initialize(local_searcher, suggestions_searcher)
-          @local_searcher = local_searcher
-          @suggestions_searcher = suggestions_searcher
-        end
+      def found_any?
+        local.any? || suggestions.any?
+      end
 
-        def found_any?
-          any_local_results? || any_suggestions_results?
-        end
+      def local
+        @local ||= Searcher::Local
+          .new(@project_name)
+          .search(including_projects: suggestions.project_names)
+      end
 
-        def exact_match
-          local_results.exact_match_project_name
-        end
-
-        def project_names
-          local_results.project_names - [exact_match]
-        end
-
-        def suggestions
-          suggestions_results.project_names - local_results.project_names
-        end
-
-        def suggestion_match
-          suggestions_results.exact_match_project_name
-        end
-
-        private
-
-        def local_results
-          @local_results ||= @local_searcher.search(suggestions_results.project_names)
-        end
-
-        def suggestions_results
-          @suggestions_results ||= @suggestions_searcher.search
-        end
-
-        def any_local_results?
-          local_results.any?
-        end
-
-        def any_suggestions_results?
-          suggestions_results.any?
-        end
+      def suggestions
+        @suggestions ||= Searcher::Rubygems
+          .new(@project_name, error_handler: on_error)
+          .search
       end
 
       module Searcher
@@ -61,91 +30,82 @@ module Ossert
             @project_name = project_name
           end
 
-          def search(suggestions_project_names = [])
+          def search(including_projects: [])
             projects = ::Project
               .where('name % ?', @project_name)
-              .or(name: suggestions_project_names)
+              .or(name: including_projects)
               .select(
                 Sequel.lit('name'),
-                Sequel.lit("name <-> #{::Project.db.literal(@project_name)} as distance"))
+                Sequel.lit("name <-> #{::Project.db.literal(@project_name)} AS distance"))
               .order(:distance)
               .limit(Results::LIMIT)
               .to_a
 
-            Results::Local.new(projects, @project_name)
+            Results.new(
+              projects,
+              match_finder: ->(project){ project[:distance].zero? }
+            )
           end
         end
 
         class Rubygems
           API_URL = 'https://rubygems.org/api/v1/search.json'.freeze
 
-          def initialize(project_name)
+          def initialize(project_name, error_handler: nil)
             @project_name = project_name
+            @error_handler = error_handler
           end
 
           def search
             response = Faraday.new.get(API_URL, query: @project_name)
 
-            if response.status == 200
-              Results::Rubygems.new(
-                JSON.parse(response.body, symbolize_names: true),
-                @project_name
-              )
-            else
-              Results::Blank.new
-            end
-          rescue Faraday::Error
-            Results::Blank.new
+            projects = if response.status == 200
+                         JSON.parse(response.body, symbolize_names: true)
+                       else
+                         []
+                       end
+
+            Results.new(
+              projects,
+              match_finder: ->(project) do
+                project[:name].downcase == @project_name.downcase
+              end
+            )
+          rescue Faraday::Error => error
+            handle_error(error)
+            Results.new([])
+          end
+
+          private
+
+          def handle_error(error)
+            @error_handler && @error_handler.call(error)
           end
         end
       end
 
-      module Results
+      class Results
         LIMIT = 15
-        class Base
-          def initialize(found_projects, project_name)
-            @found_projects = found_projects.take(LIMIT)
-            @project_name = project_name
-          end
 
-          def any?
-            !@found_projects.empty?
-          end
-
-          def project_names
-            @found_projects.map { |project| project[:name] }
-          end
-
-          def exact_match_project_name
-            exact_match && exact_match[:name]
-          end
+        def initialize(found_projects, match_finder: ->(_){ })
+          @found_projects = found_projects.take(LIMIT)
+          @match_finder = match_finder
         end
 
-        class Local < Base
-          private
-
-          def exact_match
-            @exact_match ||= @found_projects.find do |project|
-              project[:distance].zero?
-            end
-          end
+        def any?
+          !@found_projects.empty?
         end
 
-        class Rubygems < Base
-          private
-
-          # TODO not so reliable
-          def exact_match
-            @exact_match ||= @found_projects.find do |project|
-              project[:name] == @project_name.downcase
-            end
-          end
+        def exact_match
+          @exact_match ||= (@found_projects.find(&@match_finder) || {})[:name]
         end
 
-        class Blank < Base
-          def initialize(*)
-            @found_projects = []
-          end
+        def matches(except: [])
+          project_names - Array(except)
+        end
+
+        def project_names
+          @found_projects.map { |project| project[:name] }
         end
       end
     end
