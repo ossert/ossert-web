80ff0b2f4742655a8416f1d62b5c3884c356561f
Pavel Grinchenko <psdcoder@gmail.com>
2017-01-22 14:52:33 +0300

    Make sections toggleable and closed by default

---
+++ front/blocks/toggleable/index.js
@@ -0,0 +1,21 @@
+import { arrayFromNodes } from '../utils';
+import RAF from 'raf';
+
+export default function init(callback) {
+  arrayFromNodes(document.querySelectorAll('[data-toggleable]')).forEach(node => {
+    let callbackIsCalled = false;
+
+    node.querySelector('[data-toggleable-toggler').addEventListener('click', () => {
+      node.classList.toggle('toggleable_closed');
+
+      if (!node.classList.contains('toggleable_closed') && !callbackIsCalled) {
+        callbackIsCalled = true;
+        RAF(() => callback(node));
+      }
+    });
+
+    if (!node.classList.contains('toggleable_closed')) {
+      callback(node);
+    }
+  });
+}
