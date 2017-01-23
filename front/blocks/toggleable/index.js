4d52f25078c4f755f77f1f4796334a93994abd95
Pavel Grinchenko <psdcoder@gmail.com>
2017-01-23 21:11:46 +0300

    Fix selector in toggleable component

--- front/blocks/toggleable/index.js
+++ front/blocks/toggleable/index.js
@@ -5,7 +5,7 @@ export default function init(callback) {
   arrayFromNodes(document.querySelectorAll('[data-toggleable]')).forEach(node => {
     let callbackIsCalled = false;
 
-    node.querySelector('[data-toggleable-toggler').addEventListener('click', () => {
+    node.querySelector('[data-toggleable-toggler]').addEventListener('click', () => {
       node.classList.toggle('toggleable_closed');
 
       if (!node.classList.contains('toggleable_closed') && !callbackIsCalled) {
