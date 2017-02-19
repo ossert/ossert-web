d9f2195c69be0f19e3dc3909fa8a2cfc527483ef
Pavel Grinchenko <psdcoder@gmail.com>
2017-02-19 17:02:51 +0300

    Setup loaders for svg files

--- front/blocks/icon/index.js
+++ front/blocks/icon/index.js
@@ -1,1 +1,9 @@
 import './icon.pcss';
+import iconsSprite from './icons-sprite.svg';
+
+const tmpWrapper = document.createElement('div');
+
+tmpWrapper.innerHTML = iconsSprite;
+tmpWrapper.firstChild.style = 'position: absolute; width: 0; height: 0; overflow: hidden;';
+
+document.body.insertBefore(tmpWrapper.firstChild, document.body.firstChild);
