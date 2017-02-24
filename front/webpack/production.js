5d27002c3360b795bb6cadcc58951273a1d8133c
Pavel Grinchenko <psdcoder@gmail.com>
2017-02-24 23:07:06 +0300

    Inline webpack runtime to page code. It allows correctly use long-term caching for vendors

--- front/webpack/production.js
+++ front/webpack/production.js
@@ -1,25 +1,108 @@
+const path = require('path');
 const webpack = require('webpack');
+const CopyPlugin = require('copy-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const CleanPlugin = require('clean-webpack-plugin');
+const StatsWriterPlugin = require('webpack-stats-plugin').StatsWriterPlugin;
+const ChunkManifestPlugin = require('chunk-manifest-webpack-plugin');
+const ChunkHash = require('webpack-chunk-hash');
 const shared = require('./shared');
 
-module.exports = Object.assign(
-  shared.config,
-  {
-    output: Object.assign(
-      shared.config.output,
+const CWD = process.cwd();
+const SRC_DIR = path.join(CWD, 'front');
+const DEST_DIR = path.join(CWD, 'public');
+
+
+module.exports = {
+  context: SRC_DIR,
+  entry: {
+    main: './main.js',
+    shared: './shared.js'
+  },
+  output: {
+    filename: '[name]-[chunkhash].js',
+    path: DEST_DIR,
+    publicPath: '/'
+  },
+  resolve: {
+    extensions: ['.js', '.json']
+  },
+  module: {
+    rules: [
       {
-        filename: '[name]-[chunkhash].js'
+        test: /\.js$/,
+        include: SRC_DIR,
+        loader: 'babel-loader',
+        options: {
+          cacheDirectory: path.join(CWD, 'tmp', 'babel-cache')
+        }
+      },
+      {
+        test: /\.svg$/,
+        issuer: /\.js$/,
+        include: SRC_DIR,
+        use: [
+          {
+            loader: 'svg-inline-loader'
+          },
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              svgo: {
+                plugins: [
+                  { cleanupIDs: false }
+                ]
+              }
+            }
+          }
+        ]
+      },
+      {
+        test: /\.svg$/,
+        issuer: /\.p?css$/,
+        include: SRC_DIR,
+        loaders: ['svg-url-loader', 'image-webpack-loader']
+      },
+      {
+        test: /\.p?css$/,
+        include: SRC_DIR,
+        use: ExtractTextPlugin.extract(['css-loader', 'postcss-loader'])
+      },
+      {
+        test: /\.mustache$/,
+        include: SRC_DIR,
+        loader: 'mustache-loader'
       }
-    ),
-    plugins: shared.config.plugins.concat(
-      new webpack.DefinePlugin({
-        __DEVELOPMENT__: false
-      }),
-      new webpack.optimize.UglifyJsPlugin({
-        sourceMap: true
-      }),
-      new ExtractTextPlugin('styles-[contenthash:20].css')
-    ),
-    devtool: 'source-map'
-  }
-);
+    ]
+  },
+  plugins: [
+    new CleanPlugin(DEST_DIR, { root: CWD }),
+    new CopyPlugin([
+      {
+        context: path.join(SRC_DIR, 'static'),
+        from: '**/*',
+        to: DEST_DIR
+      }
+    ]),
+    new webpack.DefinePlugin({
+      __DEVELOPMENT__: false
+    }),
+    new webpack.optimize.CommonsChunkPlugin({
+      names: ['manifest', 'shared'],
+      minChunks: module => module.resource && !module.resource.includes(SRC_DIR)
+    }),
+    new webpack.HashedModuleIdsPlugin(),
+    new ChunkHash(),
+    new ExtractTextPlugin('styles-[contenthash:20].css'),
+    new ChunkManifestPlugin(),
+    new StatsWriterPlugin({
+      filename: 'assets-mapping.json',
+      fields: null,
+      transform: shared.assetsTransform
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: true
+    })
+  ],
+  devtool: 'source-map'
+};
--- front/webpack/shared.js
+++ front/webpack/shared.js
@@ -1,115 +1,22 @@
-const path = require('path');
-const webpack = require('webpack');
-const CopyPlugin = require('copy-webpack-plugin');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const CleanPlugin = require('clean-webpack-plugin');
-const StatsWriterPlugin = require('webpack-stats-plugin').StatsWriterPlugin;
+module.exports.assetsTransform = data => JSON.stringify(Object.keys(data.assetsByChunkName).reduce((result, bundle) => {
+  const bundleChunks = Array.isArray(data.assetsByChunkName[bundle])
+    ? data.assetsByChunkName[bundle]
+    : [data.assetsByChunkName[bundle]];
 
-const CWD = process.cwd();
-const SRC_DIR = path.join(CWD, 'front');
-const DEST_DIR = path.join(CWD, 'public');
+  const jsAssets = bundleChunks.filter(filterByExt('js'));
+  const cssAssets = bundleChunks.filter(filterByExt('css'));
 
-module.exports.SRC_DIR = SRC_DIR;
-module.exports.DEST_DIR = DEST_DIR;
+  if (jsAssets.length) {
+    result.bundles[bundle] = jsAssets[0];
+  }
 
-module.exports.config = {
-  context: SRC_DIR,
-  entry: {
-    main: './main.js',
-    shared: 'babel-polyfill'
-  },
-  output: {
-    path: DEST_DIR,
-    publicPath: '/'
-  },
-  resolve: {
-    extensions: ['.js', '.json']
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        include: SRC_DIR,
-        loader: 'babel-loader',
-        options: {
-          cacheDirectory: path.join(CWD, 'tmp', 'babel-cache')
-        }
-      },
-      {
-        test: /\.svg$/,
-        issuer: /\.js$/,
-        include: SRC_DIR,
-        use: [
-          {
-            loader: 'svg-inline-loader'
-          },
-          {
-            loader: 'image-webpack-loader',
-            options: {
-              svgo: {
-                plugins: [
-                  { cleanupIDs: false }
-                ]
-              }
-            }
-          }
-        ]
-      },
-      {
-        test: /\.svg$/,
-        issuer: /\.p?css$/,
-        include: SRC_DIR,
-        loaders: ['svg-url-loader', 'image-webpack-loader']
-      },
-      {
-        test: /\.p?css$/,
-        include: SRC_DIR,
-        use: ExtractTextPlugin.extract(['css-loader', 'postcss-loader'])
-      },
-      {
-        test: /\.mustache$/,
-        include: SRC_DIR,
-        loader: 'mustache-loader'
-      }
-    ]
-  },
-  plugins: [
-    new CleanPlugin(DEST_DIR, { root: CWD }),
-    new CopyPlugin([
-      {
-        context: path.join(SRC_DIR, 'static'),
-        from: '**/*',
-        to: DEST_DIR
-      }
-    ]),
-    new StatsWriterPlugin({
-      filename: 'stats.json',
-      fields: null,
-      transform: data => JSON.stringify(Object.keys(data.assetsByChunkName).reduce((result, bundle) => {
-        const bundleChunks = Array.isArray(data.assetsByChunkName[bundle])
-          ? data.assetsByChunkName[bundle]
-          : [data.assetsByChunkName[bundle]];
+  if (cssAssets.length) {
+    result.css = cssAssets[0];
+  }
 
-        const jsAssets = bundleChunks.filter(filterByExt('js'));
-        const cssAssets = bundleChunks.filter(filterByExt('css'));
+  return result;
+}, { bundles: {}, css: null }));
 
-        if (jsAssets.length) {
-          result.bundles[bundle] = jsAssets[0];
-        }
-
-        if (cssAssets.length) {
-          result.css = cssAssets[0];
-        }
-
-        return result;
-      }, { bundles: {}, css: null }))
-    }),
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'shared',
-      minChunks: module => module.resource && !module.resource.includes(SRC_DIR)
-    })
-  ]
-};
 
 function filterByExt(ext) {
   return value => (new RegExp(`\\.${ext}$`)).test(value);
